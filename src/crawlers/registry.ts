f6855f1a46dde4d2b9f95c200e808314baa79215
Raí <raivitor@gmail.com>
2025-09-20 10:20:32 -0300

    feat: lista atualizada

--- src/crawlers/registry.ts
+++ src/crawlers/registry.ts
@@ -4,17 +4,23 @@ import creditoRealCrawler from '@/crawlers/providers/credito-real';
 import daltonCrawler from '@/crawlers/providers/dalton';
 import ibagyCrawler from '@/crawlers/providers/ibagy';
 import olxCrawler from '@/crawlers/providers/olx';
+import gralhaCrawler from '@/crawlers/providers/gralha';
+import sanRemoCrawler from '@/crawlers/providers/Sanremo';
 
 export const crawlers: BaseCrawler[] = [
   creditoRealCrawler,
   ibagyCrawler,
   olxCrawler,
+  gralhaCrawler,
   daltonCrawler,
+  sanRemoCrawler,
 ];
 
 export { creditoRealCrawler };
 export { ibagyCrawler };
 export { olxCrawler };
 export { daltonCrawler };
+export { gralhaCrawler };
+export { sanRemoCrawler };
 
 export default crawlers;
